6d2a002ff22779c008a9da779a60bde9e41ba79c
Nirea-ithiliel <34683033+Nirea-ithiliel@users.noreply.github.com>
2019-06-25 10:56:43 +0300

    1

--- addressbook-web-tests/addressbook-web-tests/model/GroupData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/GroupData.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToDB.Mapping;
 
 namespace WebAddressbookTests
 {
+    [Table(Name = "group_list")]
     public class GroupData : IEquatable<GroupData>, IComparable<GroupData>
     {
         public GroupData()
@@ -40,7 +42,7 @@ namespace WebAddressbookTests
         {
             return "name=" + Name + "\nheader= " + Header + "\nfooter=" + Footer;
         }
-       
+
         public int CompareTo(GroupData other)
         {
             if (Object.ReferenceEquals(other, null))
@@ -50,12 +52,24 @@ namespace WebAddressbookTests
             return Name.CompareTo(other.Name);
         }
 
-        public string Name { get; set;  }
+        [Column(Name = "group_name")]
+        public string Name { get; set; }
 
+        [Column(Name = "group_header")]
         public string Header { get; set; }
-        
+
+        [Column(Name = "group_footer")]
         public string Footer { get; set; }
 
+        [Column(Name = "group_id"), PrimaryKey, Identity]
         public string Id { get; set; }
+
+        public List<GroupData> GetAll()
+        {
+            using (AddressBookDB db = new AddressBookDB())
+            {
+                return (from g in db.Groups select g).ToList();
+            }
+        }
     }
 }
--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -9,6 +9,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Linq;
 
 namespace WebAddressbookTests
 {
@@ -117,6 +118,20 @@ namespace WebAddressbookTests
             oldGroups.Sort();
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
+        }
+
+        [Test]
+        public void TestDBConnectivity()
+        {
+            DateTime start = DateTime.Now;
+            List<GroupData> fromUi = app.Groups.GetGroupList();
+            DateTime end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
+
+            start = DateTime.Now;
+            List<GroupData> fromDb = GroupData.GetAll();
+              end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
         }
     }
 }
